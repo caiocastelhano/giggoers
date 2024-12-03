2836e2d69940e5f853a239ce573733399db10f4a
Virginie Genes <virginiegenes@hotmail.com>
2024-12-03 16:20:09 -0300

    created 3 buttons on the footer

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -9,3 +9,4 @@ class Venue < ApplicationRecord
   validates :capacity, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
   validates :url_image, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), allow_blank: true }
 end
+

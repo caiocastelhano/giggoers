25447301c8aa8951802f7754e41fe13df092d24d
Raquel Sylos <raquelsylos@gmail.com>
2024-12-03 12:31:48 -0300

    fixing name on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,12 @@
 class User < ApplicationRecord
   has_many :favorites
   has_many :events, through: :favorites
-  
+
   # Validações
-  validates :nome, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6 }
-  
-  # Segurança para a senha
-  has_secure_password
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

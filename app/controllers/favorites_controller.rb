64903ab534419e27004d33f9d230611a6b72c5ab
Virginie Genes <virginiegenes@hotmail.com>
2024-12-03 18:27:42 -0300

    started coding show favoritos but could not test

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,7 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user! # Ensure the user is logged in
+
+  def index
+    @favorites = current_user.favorites.includes(:event) # Fetch user's favorites with associated events
+  end
 end

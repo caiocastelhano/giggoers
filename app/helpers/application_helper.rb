b2617cdc5461b97a8b264801c777fac3b98bf973
Virginie Genes <virginiegenes@hotmail.com>
2024-12-10 15:24:54 -0300

    authorize user to delete its register

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-  def bootstrap_class_for_flash(type)
-    case type
-    when "notice" then "alert-success"
-    when "alert" then "alert-danger"
-    when "error" then "alert-warning"
-    else "alert-info"
-    end
-  end
 end
-

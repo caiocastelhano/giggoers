69b232d8a351e844d444273effa3883ad098612c
Maira Almeida <mairasenna@gmail.com>
2024-12-10 11:50:37 -0300

    perfil criado

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,18 @@ class ApplicationController < ActionController::Base
   # Configura os parâmetros permitidos para Devise antes de qualquer ação do Devise ser chamada.
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # Inclui helpers do Devise
+  include Devise::Controllers::Helpers
+
+  helper_method :user_signed_in?, :current_user
+
+  before_action :authenticate_user! # Garante que o usuário esteja autenticado por padrão
+
+  # Redireciona para a tela de login após o logout
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path # Página de login
+  end
+
   protected
 
   # Este método adiciona parâmetros extras que o Devise deve aceitar no cadastro e atualização de usuários.

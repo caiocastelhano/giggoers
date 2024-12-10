2a3cb8b44e8b7d420b59546a38eff0d587352464
Maira Almeida <mairasenna@gmail.com>
2024-12-10 13:56:30 -0300

    profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
+
 
   devise_scope :user do
     get 'profile', to: 'users#show', as: :user_profile

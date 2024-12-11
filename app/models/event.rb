a02192ee1c2a056fd27527d7a1bf410b419025ee
Maira Almeida <mairasenna@gmail.com>
2024-12-11 12:08:13 -0300

    mensagens removidas

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,27 +1,3 @@
-def self.search(query, sort_by = 'created_at')
-    events = joins(:genres, :venue)
-      .where(
-        "events.title ILIKE :query OR
-         events.description ILIKE :query OR
-         venues.name ILIKE :query OR
-         genres.name ILIKE :query",
-        query: "%#{query}%"
-      )
-      .distinct
-      .select('events.*, genres.name AS genre_name')  # Incluindo a coluna genres.name na seleção
-
-    case sort_by
-    when 'data'
-      events.order(start_date: :asc)
-    when 'preco'
-      events.order(price: :asc)
-    when 'tipo'
-      events.order('genre_name ASC')  # Agora você pode ordenar por genre_name
-    else
-      events.order(created_at: :desc)
-    end
-  end
-
 class Event < ApplicationRecord
   belongs_to :venue
   has_many :favorites
@@ -35,4 +11,33 @@ class Event < ApplicationRecord
   # Atributos opcionais
   validates :price, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
   validates :url_image, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), allow_blank: true }
+
+  # Método de busca
+  def self.search(query, sort_by = 'created_at')
+    return all unless query.present?
+
+    # Usa DISTINCT para evitar duplicados
+    events = joins(:genres, :venue)
+      .where(
+        "events.title ILIKE :query OR
+         events.description ILIKE :query OR
+         venues.name ILIKE :query OR
+         genres.name ILIKE :query",
+        query: "%#{query}%"
+      )
+      .distinct  # Garante que os resultados sejam únicos
+      .select('events.*, genres.name AS genre_name') # Seleção adicional
+
+    # Ordenação com base no parâmetro sort_by
+    case sort_by
+    when 'data'
+      events.order(start_date: :asc)
+    when 'preco'
+      events.order(price: :asc)
+    when 'tipo'
+      events.order('genre_name ASC')
+    else
+      events.order(created_at: :desc)
+    end
+  end
 end
